45a14cd9d3c5a4aea3f5005157b928a1ae87509a
TrymoretimeH <pvhcnptit@gmail.com>
2023-03-23 20:23:09 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars') 
-const app = express()
-const port = 3000
+const route = require('./routes');
 
-const route = require('./routes')
+app.use(express.static('src/public'));
 
-app.use(express.static('src/public'))
-
-app.use(express.urlencoded({
-  extended: true,
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // HTTP logger
 // app.use(morgan('combined'))
 
 // Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', 'src/resources/views')
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', 'src/resources/views');
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
