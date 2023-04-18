c761a49946eaf5f4cdd1888ef5270659ee0cc3df
TrymoretimeH <pvhcnptit@gmail.com>
2023-04-18 11:37:32 +0700

    Create on CRUD

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,17 +1,16 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
     index(req, res, next) {
         Course.find({})
             .then((courses) => {
-                res.json(courses);
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses),
+                });
             })
-            .catch((err) => {
-                res.status(400).json({ error: 'ERROR!!!' });
-                console.log(err);
-            });
-        // res.render('home');
+            .catch(next);
     }
 
     // [GET] /search
