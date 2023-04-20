41bf320a110ec670d940ab528d7ab2cfc46fe635
TrymoretimeH <pvhcnptit@gmail.com>
2023-04-20 10:12:30 +0700

    Update and Delete on [CRUD]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -19,6 +20,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP logger
 // app.use(morgan('combined'))
 
@@ -27,6 +30,9 @@ app.engine(
     'hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
